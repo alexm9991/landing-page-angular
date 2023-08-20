92458d405ce8aee96cd15dadad4dd109d0f7edfb
Alexandra Merchan Cardenas <geralex01@gmail.com>
2023-08-20 04:08:42 -0500

    :feat: :sparkles: HomeComponent base

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -2,12 +2,26 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
+import { HeaderComponent } from './components/header/header.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { ServicesComponent } from './components/services/services.component';
+import { PlansComponent } from './components/plans/plans.component';
+import { UsComponent } from './components/us/us.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/header/navbar.component';
 
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
+    NavbarComponent,
+    BannerComponent,
+    ServicesComponent,
+    PlansComponent,
+    UsComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule,
