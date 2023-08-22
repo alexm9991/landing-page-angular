3f7dfa399c17a870a9dfe0452b5180140d48c229
Alexandra Merchan Cardenas <geralex01@gmail.com>
2023-08-22 00:45:17 -0500

    fix: :bug: Error strict-origin-when-cross-origin
    http -> https

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  api: 'http://jsonplaceholder.typicode.com'
+  api: 'https://jsonplaceholder.typicode.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  api: 'http://jsonplaceholder.typicode.com'
+  api: 'https://jsonplaceholder.typicode.com'
 };
